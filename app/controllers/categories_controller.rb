e272d505306d7abe598b5a37b2efb6908c4d6f9e
clintonjosephs <clintonjosephs26@gmail.com>
2022-05-23 23:51:06 +0100

    created form for adding new categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,27 @@
 class CategoriesController < ApplicationController
+    include FlashMsgHelper
     before_action :authenticate_user!
     
     def index; end
+
+    def new
+        @category = Category.new
+    end
+
+    def create
+        @category = Category.new(category_params)
+        @category.user_id = current_user.id
+        if @category.save
+          success('New category was successfully created.', true)
+          redirect_to authenticated_root_path
+        else
+          failure('Category was not created because: ', @category)
+          render :new
+        end
+    end
+
+
+    def category_params
+        params.require(:category).permit(:name, :image)
+    end
 end
--- app/helpers/flash_msg_helper.rb
+++ app/helpers/flash_msg_helper.rb
@@ -1,11 +1,15 @@
 module FlashMsgHelper
-    def success(msg)
-        flash.now[:success] = msg
+    def success(msg, redirect = false)
+        if redirect
+            flash[:success] = msg
+        else
+            flash.now[:success] = msg
+        end
     end
 
-    def failure(msg)
+    def failure(msg, obj)
         flash.now[:danger] = msg + '<ul class="error-list">'
-        new_food.errors.full_messages.each do |msg|
+        obj.errors.full_messages.each do |msg|
           flash.now[:danger] += "<li>#{msg}</li>"
         end
         flash.now[:danger] += '</ul>'
