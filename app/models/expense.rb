9d63b60c84df76c2a89f482f675ffaa8332f518b
clintonjosephs <clintonjosephs26@gmail.com>
2022-05-23 10:23:24 +0100

    implemented validations in expenses model

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,4 +1,9 @@
 class Expense < ApplicationRecord
   belongs_to :user
   has_many :categories through: :expense_categories, dependent: :destroy
+
+  validates :name, presence: true, maxlength: 200
+  validates :amount, presence: true, numericality: { greater_than: 0, less_than: 100000000 }
+  validates :user_id, presence: true
+
 end
