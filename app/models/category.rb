5aa756b20c72b76eb6d0a301de3a4f3c55868d91
clintonjosephs <clintonjosephs26@gmail.com>
2022-05-23 10:20:55 +0100

    implemented validations in users model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,11 +1,15 @@
 class Category < ApplicationRecord
   include ImageHelper
 
+  # relationships
   belongs_to :user
   has_one_attached :image, :dependent => :destroy
   has_many :expenses through: :expense_categories, dependent: :destroy
+
+  # validations
   validates :name, presence: true, uniqueness: { scope: :user_id }
   validates :user_id, presence: true
   validates :image_type
   validates :image_size
+ 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,17 @@
 class User < ApplicationRecord
+  include ImageHelper
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+
+  # relationships
   has_one_attached :image
   has_many :categories, dependent: :destroy
   has_many :expenses, dependent: :destroy
+
+  # validations
+  validates :name, presence: true
+  validates :image_type
+  validates :image_size
 end
