7962548a89c209437171f7198c382bfc441e4c6e
clintonjosephs <clintonjosephs26@gmail.com>
2022-05-24 13:39:40 +0100

    implemented the list of categories in the home view

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,7 +2,17 @@ class CategoriesController < ApplicationController
   include FlashMsgHelper
   before_action :authenticate_user!
 
-  def index; end
+  def index
+    @categories = Category.with_attached_image.find_by_sql("SELECT categories.*, 
+      SUM(expenses.amount) as total_amount 
+      from categories 
+      LEFT JOIN categories_expenses ON categories.id = categories_expenses.category_id 
+      LEFT JOIN expenses on categories_expenses.expense_id = expenses.id 
+      WHERE categories.user_id = #{current_user.id} GROUP BY categories.id");
+
+    # @current_month_amount = @categories.where('extract(month from date_of_expense) = ?', Time.now.month).sum(:total_amount)
+    # @highest_month = @categories.where('extract(month from date_of_expense) = ?', Time.now.month).order('total_amount DESC').first
+  end
 
   def new
     @category = Category.new
